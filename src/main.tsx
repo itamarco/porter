dbe29e1a824fa0101efb5065068d7ab8e62b1db9
itamarco <itc404@gmail.com>
2026-01-08 21:03:16 +0200

    font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 
 if (!window.electronAPI) {
   console.warn('window.electronAPI is not available. Make sure you are running in Electron.');
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       colors: {
         skeuo: {
           bg: '#2e323b',
